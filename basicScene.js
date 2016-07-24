d4d5b93d85fbcb48b2d99030b31eb6c6cdd77ac5
k6e2n0t12 <k6e2n0t12@gmail.com>
2016-07-24 19:52:28 +0800

    up

--- basicScene.js
+++ basicScene.js
@@ -1,10 +1,10 @@
 var basicScene = function (scene){
 	
 	console.log(scene);
-	var floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(16,8,32),new THREE.MeshLambertMaterial({
-	side: THREE.DoubleSide,
-	color: 0xffffff
+	this.floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(16,8,32),new THREE.MeshLambertMaterial({
+		side: THREE.DoubleSide,
+		color: 0xffffff
 	}));
-	floor.rotation.x=-Math.PI/2;
-	scene.add(floor);
+	this.floor.rotation.x=-Math.PI/2;
+	scene.add(this.floor);
 };

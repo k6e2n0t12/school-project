1f83470212343101530f364b67a84867464e4179
k6e2n0t12 <k6e2n0t12@gmail.com>
2016-07-24 19:34:22 +0800

    update

---
+++ basicScene.js
@@ -0,0 +1,8 @@
+var basicScene = fuction (scene){
+	floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(16,8,32),new THREE.MeshLambertMaterial({
+	side: THREE.DoubleSide,
+	color: 0xffffff
+	}));
+	floor.rotation.x=-Math.PI/2;
+	scene.add(floor);
+}
